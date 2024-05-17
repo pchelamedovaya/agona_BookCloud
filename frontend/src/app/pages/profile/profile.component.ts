e4948e4e02071185d48a29732cfcbe3419c00b34
Regina <tuturiw@mail.ru>
2024-05-17 21:47:14 +0300

    add profile, search page

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,11 +1,96 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	ChangeDetectorRef,
+	Component,
+	OnInit
+} from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { UserService } from '../../services/user.service'
+import { IUser } from '../../types/user.interface'
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-profile',
+	templateUrl: './profile.component.html',
+	styleUrl: './profile.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+	profileData: FormGroup
+	savedData: any = {}
+	EnterInfo = false
+	ButtonVisibility = false
+	userDataLoaded = false
 
+	ngOnInit() {
+		this.userService.getCurrentUserInfo().subscribe({
+			next: (user: IUser) => {
+				// console.log('Current user info:', user)
+				if (
+					user.firstName &&
+					user.lastName &&
+					user.about &&
+					user.favouriteGenres
+				) {
+					this.savedData = user
+					this.userDataLoaded = true
+					this.cdr.markForCheck()
+				} else {
+					console.log('User data is incomplete:', user)
+				}
+			},
+			error: error => {
+				console.error('Error getting current user info:', error)
+			}
+		})
+	}
+
+	constructor(
+		private readonly userService: UserService,
+		private readonly cdr: ChangeDetectorRef
+	) {
+		this.profileData = new FormGroup({
+			firstName: new FormControl('', [
+				Validators.pattern(/^[a-zA-Zа-яА-Я]+(?:-[a-zA-Zа-яА-Я]+)*$/)
+			]),
+			lastName: new FormControl('', [
+				Validators.pattern(/^[a-zA-Zа-яА-Я]+(?:-[a-zA-Zа-яА-Я]+)*$/)
+			]),
+			about: new FormControl('', [Validators.maxLength(50)]),
+			favouriteGenres: new FormControl('', [])
+		})
+	}
+
+	onSubmit() {
+		const formData = {
+			firstName: this.profileData.get('firstName')?.value,
+			lastName: this.profileData.get('lastName')?.value,
+			about: this.profileData.get('about')?.value,
+			favouriteGenres: this.profileData.get('favouriteGenres')?.value
+		}
+		if (this.profileData.valid) {
+			this.userService.updateUser(formData).subscribe({
+				next: user => {
+					// console.log('User updated successfully:', user)
+				},
+				error: error => {
+					console.error('Error updating user:', error)
+				}
+			})
+			this.savedData = { ...this.profileData.value }
+		} else {
+			console.log('Data is not valid')
+		}
+	}
+
+	onEnter() {
+		this.EnterInfo = !this.EnterInfo
+	}
+
+	onInput() {
+		this.ButtonVisibility = true
+	}
+
+  showFieldsForEdit() {
+    this.userDataLoaded = false
+  }
 }
--- frontend/src/app/types/book.interface.ts
+++ frontend/src/app/types/book.interface.ts
@@ -1,9 +1,17 @@
 export interface IBook {
-  id: number
-  title: string
-  author: string
-  description: string
-  cover: string
-  genre: string
-  reading_link: string
+	id: number
+	title: string
+	author: string
+	description: string
+	cover: string
+	genre: string
+	reading_link: string
+}
+
+export interface IBooksResponse {
+  data: IBook[];
+  page: number;
+  size: number;
+  totalElements: number;
+  totalPages: number;
 }
